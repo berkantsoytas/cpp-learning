eb150a2be49f98247a6fc0dbb2e415f7da01f145
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-08-30 21:09:13 +0300

    HomeWork1

---
+++ Work1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  std::string country;
+
+  std::cout << "Where do you live?" << std::endl;
+  std::cin >> country;
+  std::cout << "I've heard great thing about " << country << ". I'd like to go sometime.";
+
+  return 0;
+}
