4ea8eec7446e6856eadc26791a669dbab5a84f6e
Berkant <54627016+berkantsoytas@users.noreply.github.com>
2022-08-30 21:08:10 +0300

    Data Output Exercise

---
+++ DataOutpuExercise/exercise.cpp
@@ -0,0 +1,14 @@
+#include "exercise.h"
+#include <iostream>
+
+void say_something()
+{
+  std::cout << "I am Berkant, 20 years old and Junior developer!" << std::endl;
+}
+
+int main()
+{
+  say_something();
+
+  return 0;
+}
